c3955d0e168b92c5864d6d02a04080952d5e7264
github-shwenakak <github-shwenakak@gmail.com>
2021-04-08 09:08:26 -0700

    Added the empty input test case

--- test.cpp
+++ test.cpp
@@ -23,6 +23,11 @@ TEST(helloTest, HelloHi) {
     EXPECT_EQ("hello hi", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
